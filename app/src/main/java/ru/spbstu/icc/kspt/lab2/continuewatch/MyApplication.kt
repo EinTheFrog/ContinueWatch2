e1f369daf6f61f7bbfac62e9c73772c0ccd111e9
EinTheFrog <MaximAlpatski@gmail.com>
2021-12-13 10:58:52 +0300

    Application fix

--- app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MyApplication.kt
+++ app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MyApplication.kt
@@ -1,10 +1,17 @@
 package ru.spbstu.icc.kspt.lab2.continuewatch
 
 import android.app.Application
+import android.util.Log
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
-object MyApplication : Application() {
-    val executor: ExecutorService = Executors.newFixedThreadPool(1)
+class MyApplication : Application() {
+    companion object {
+        val executor: ExecutorService = Executors.newFixedThreadPool(1)
+    }
 
+    override fun onCreate() {
+        super.onCreate()
+        Log.d("Debug", "Application created")
+    }
 }
